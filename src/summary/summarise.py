93e2a9abec65005cde0aaa6be9e7d7a7ffb66c63
bobturneruk <bob.turner.uk@gmail.com>
2023-07-25 13:13:27 +0100

    Better name

--- src/summary/summarise.py
+++ src/summary/summarise.py
@@ -305,11 +305,11 @@ def summarise() -> None:
     parser.add_argument(
         "--output_path",
         default="Mega.json",
-        dest="output_path",
+        dest="output",
         help="Path including name for output .json file",
     )
     args = parser.parse_args()
     summary = create_summary(
         args.gatekeeper, args.mapping, args.mykrobe, args.gnomonicus
     )
-    write_summary(summary, args.output_path)
+    write_summary(summary, args.output)
