78e1016cf4eaa977c4e36c2d7bddfa678b910e8b
bobturneruk <bob.turner.uk@gmail.com>
2023-08-01 23:44:30 +0100

    Parameterise regression test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,31 +1,23 @@
 import pytest
 from pathlib import Path
 
+WTCHG_885333_73225298_1_1 = {
+    "gatekeeper_report": "test_data/example_input/WTCHG_885333_73225298_1/1/gatekeeper_report.json",
+    "competitivemapping_report": "test_data/example_input/WTCHG_885333_73225298_1/1/competitivemapping_report.json",
+    "mykrobe_report": "test_data/example_input/WTCHG_885333_73225298_1/1/mykrobe_report.json",
+    "gnomonicus": "test_data/example_input/WTCHG_885333_73225298_1/1/tb/gnomonicus.json",
+    "expected_output": "test_data/example_output/WTCHG_885333_73225298_1/1/Mega_1.json",
+}
 
-@pytest.fixture
-def gatekeeper_report() -> Path:
-    return Path(
-        "test_data/example_input/WTCHG_885333_73225298_1/2/gatekeeper_report.json"
-    )
-
-
-@pytest.fixture
-def competitivemapping_report() -> Path:
-    return Path(
-        "test_data/example_input/WTCHG_885333_73225298_1/2/competitivemapping_report.json"
-    )
-
-
-@pytest.fixture
-def mykrobe_report() -> Path:
-    return Path("test_data/example_input/WTCHG_885333_73225298_1/2/mykrobe_report.json")
+WTCHG_885333_73225298_1_2 = {
+    "gatekeeper_report": "test_data/example_input/WTCHG_885333_73225298_1/2/gatekeeper_report.json",
+    "competitivemapping_report": "test_data/example_input/WTCHG_885333_73225298_1/2/competitivemapping_report.json",
+    "mykrobe_report": "test_data/example_input/WTCHG_885333_73225298_1/2/mykrobe_report.json",
+    "gnomonicus": "test_data/example_input/WTCHG_885333_73225298_1/2/tb/gnomonicus.json",
+    "expected_output": "test_data/example_output/WTCHG_885333_73225298_1/2/Mega_2.json",
+}
 
 
-@pytest.fixture
-def gnomonicus() -> Path:
-    return "test_data/example_input/WTCHG_885333_73225298_1/2/tb/gnomonicus.json"
-
-
-@pytest.fixture
-def expected_output() -> Path:
-    return "test_data/example_output/WTCHG_885333_73225298_1/2/Mega_2.json"
+@pytest.fixture(params=[WTCHG_885333_73225298_1_1, WTCHG_885333_73225298_1_2])
+def test_set(request) -> dict:
+    return request.param
