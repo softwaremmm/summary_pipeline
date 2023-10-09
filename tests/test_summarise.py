d4973ec3084a38bbd2bd2ed3d2e0c7dc258ee79f
bobturneruk <bob.turner.uk@gmail.com>
2023-10-09 14:37:34 +0100

    tests: Update TB name

--- tests/test_summarise.py
+++ tests/test_summarise.py
@@ -31,7 +31,7 @@ def test_generate_sequencing_quality(
     eg_competitivemapping_report_contents, eg_clockwork_report_contents
 ) -> None:
     expected_sq_output = {
-        "Mapped To": "Mycobacterium tuberculosis H37Rv complete genome",
+        "Mapped To": "M.tuberculosis",
         "Num Reads": 3953463.0,
         "Coverage": 98.132,
         "Mean Depth": 132.507,
