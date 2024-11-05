a05afb44c7480d7b645433acd4b435e038f8b3da
Matthew Colpus <mpcolpus@gmail.com>
2024-11-05 13:34:09 +0000

    fix: add pytest just for naming

--- tests/conftest.py
+++ tests/conftest.py
@@ -505,3 +505,7 @@ def eg_gnomonicus_report() -> Path:
 def eg_gnomonicus_report_contents(eg_gnomonicus_report) -> dict:
     with open(eg_gnomonicus_report, "r") as file:
         return json.load(file)
+
+@pytest.fixture
+def name_mapping() -> Path:
+    return Path("test_data/reference/name_mapping.csv")
